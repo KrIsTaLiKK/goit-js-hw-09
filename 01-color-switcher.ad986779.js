958a407253d5aa0b28c9ee4587d18e587b409d8d
KrIsTaLiKK <99838989+KrIsTaLiKK@users.noreply.github.com>
2023-11-25 18:45:54 +0000

    Deploying to gh-pages from @ KrIsTaLiKK/goit-js-hw-09@ab55beeb3979bfe1b1ce2bf8b03d5b893ceeb6f7 🚀

---
+++ 01-color-switcher.ad986779.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body,o=null;t.addEventListener("click",(function(){o=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),n.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.ad986779.js.map
